d049cf563b990ccd55c67a02074a4ba5fc024718
Andrey Rebelatto <lzzgrip@gmail.com>
2023-10-12 19:49:37 -0300

    Response Entity
    aplicação de metodos get e set, Response Entity e inclusao do H2 DB

--- src/main/java/com/andreyyon/todolist/user/UserController.java
+++ src/main/java/com/andreyyon/todolist/user/UserController.java
@@ -1,37 +1,32 @@
 package com.andreyyon.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * Modificador ~
- * public
- * private
- * protected
- */
-
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
-    /**
-     * String
-     * Intenger
-     * Double
-     * Boolean
-     * Float
-     * char
-     * Date
-     * void
-     */
-     /*
-      * Body
-      */
-      
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/")
-    public void create(@RequestBody UserModel UserModel){
-        System.out.println(UserModel.getUsername());
+    public ResponseEntity create(@RequestBody UserModel userModel){
+      var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if(user != null){
+            System.out.println("usuario ja existe");
+            //mensagem de erro
+            //status code
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuario já existe");
+        }
+
+      var createdUser = this.userRepository.save(userModel);
+      return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
     }
 }
